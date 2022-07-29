5251dab316cd39bf68399902cce7ab9465c58e72
Andrewbui389 <andrewbui389@gmail.com>
2022-07-28 19:53:06 -0700

    fixing styling main screen admin

--- controllers/staff.js
+++ controllers/staff.js
@@ -11,11 +11,11 @@ async function index(req , res) {
         return res.redirect('/');
     };
     if(req.user.Admin === true) return res.redirect('/admin')
-    const date = new Date()
-    const templateCurrMonth = `${date.getMonth() + 1}/1/${date.getFullYear()}`
-    let data = await DayOf.findOne({clockedOut: null , name : req.user.name})
-    let employeeData = await DayOf.find({createdAt : {$gte : templateCurrMonth} , user : req.user._id })
     try {
+        const date = new Date()
+        const templateCurrMonth = `${date.getMonth() + 1}/1/${date.getFullYear()}`
+        let data = await DayOf.findOne({clockedOut: null , name : req.user.name})
+        let employeeData = await DayOf.find({createdAt : {$gte : templateCurrMonth} , user : req.user._id })
         return res.render('./Staff/index' , {data , employeeData});
     } catch (error) {
         return res.redirect('/')
